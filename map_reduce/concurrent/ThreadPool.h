fbecaac7aff9404b82775aef01a3768c265d6ced
sabotzs <gkuklev98@gmail.com>
2022-07-19 21:59:30 +0300

    Add private methods for task management

--- map_reduce/concurrent/ThreadPool.h
+++ map_reduce/concurrent/ThreadPool.h
@@ -13,6 +13,22 @@ namespace mr::concurrent::detail {
 	class ThreadPool {
 		using Task = std::function<void()>;
 	private:
+		void push_task(Task&& task) {
+			{
+				std::lock_guard<std::mutex> lock(mtx);
+				task_queue.push(std::move(task));
+			}
+			cv.notify_one();
+		}
+
+		void pop_task(Task& task) {
+			std::unique_lock<std::mutex> lock(mtx);
+			cv.wait(lock, [this] {return !task_queue.empty() || done; });
+			if (done) return;
+			task = std::move(task_queue.front());
+			task_queue.pop();
+		}
+	private:
 		std::atomic_bool done = false;
 		std::vector<std::thread> threads;
 		std::queue<Task> task_queue;
