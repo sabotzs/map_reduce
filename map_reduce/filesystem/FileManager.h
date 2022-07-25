62bbd1b57d0f8184760907039766f770ed4d729a
sabotzs <gkuklev98@gmail.com>
2022-07-25 17:45:16 +0300

    Add opening in read mode for temporary files

--- map_reduce/filesystem/FileManager.h
+++ map_reduce/filesystem/FileManager.h
@@ -9,7 +9,7 @@
 
 namespace mr::filesystem::detail {
 	class FileManager {
-		using TempFiles = std::vector<std::shared_ptr<std::ofstream>>;
+		using TempFiles = std::vector<std::shared_ptr<std::fstream>>;
 	public:
 		FileManager(const std::string tmp_dir, size_t partitions)
 			: tmp_dir(tmp_dir)
@@ -22,7 +22,7 @@ namespace mr::filesystem::detail {
 			std::filesystem::create_directory(this->tmp_dir);
 		}
 
-		void create_tmp_files_for(const std::string& filename) {
+		void open_tmp_files_for_writing(const std::string& filename) {
 			std::string file_tmp_dir = tmp_dir + filename + '/';
 			std::string tmp_file_basename = file_tmp_dir + filename + "-";
 
@@ -31,9 +31,31 @@ namespace mr::filesystem::detail {
 			tmp_files[filename] = {};
 			tmp_files[filename].reserve(partitions);
 			for (size_t i = 0; i < partitions; ++i) {
-				auto ofs = std::make_shared<std::ofstream>(tmp_file_basename + std::to_string(i));
+				auto name = tmp_file_basename + std::to_string(i);
+				auto ofs = std::make_shared<std::fstream>(name, std::ios::out);
 				tmp_files[filename].emplace_back(ofs);
 			}
+		}
+
+		void close_tmp_files_for_writing(const std::string& filename) {
+			tmp_files.erase(filename);
+		}
+
+		void open_tmp_files_for_reading(const std::string& filename) {
+			std::string file_tmp_dir = tmp_dir + filename + '/';
+			std::string tmp_file_basename = file_tmp_dir + filename + "-";
+
+			tmp_files[filename] = {};
+			tmp_files[filename].reserve(partitions);
+			for (size_t i = 0; i < partitions; ++i) {
+				auto name = tmp_file_basename + std::to_string(i);
+				auto ifs = std::make_shared<std::fstream>(name, std::ios::in);
+				tmp_files[filename].emplace_back(ifs);
+			}
+		}
+
+		void close_tmp_files_for_reading(const std::string& filename) {
+			tmp_files.erase(filename);
 		}
 
 		TempFiles& tmp_files_for(const std::string& filename) {
