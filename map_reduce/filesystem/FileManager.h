f7553ca27876e05f5bd94eaeddcbf8da471aed2c
sabotzs <gkuklev98@gmail.com>
2022-07-26 16:18:58 +0300

    Create all files in construction. Delete temporary files on destruction

--- map_reduce/filesystem/FileManager.h
+++ map_reduce/filesystem/FileManager.h
@@ -6,65 +6,74 @@
 #include <fstream>
 #include <filesystem>
 #include <unordered_map>
+#include "core/Configuration.h"
 
 namespace mr::filesystem::detail {
 	class FileManager {
-		using TempFiles = std::vector<std::shared_ptr<std::fstream>>;
+		using Configuration = mr::core::detail::Configuration;
+		using TmpFiles = std::vector<std::shared_ptr<std::fstream>>;
+		using OutFile = std::shared_ptr<std::ofstream>;
 	public:
-		FileManager(const std::string tmp_dir, size_t partitions)
-			: tmp_dir(tmp_dir)
-			, partitions(partitions)
-			, tmp_files()
+		FileManager(const Configuration& config)
+			: tmp_dir(config.tmp_dir)
 		{
-			if (!std::filesystem::is_directory(this->tmp_dir)) {
-				this->tmp_dir += '/';
+			if (!tmp_dir.ends_with('/')) {
+				tmp_dir += '/';
 			}
-			std::filesystem::create_directory(this->tmp_dir);
-		}
-
-		void open_tmp_files_for_writing(const std::string& filename) {
-			std::string file_tmp_dir = tmp_dir + filename + '/';
-			std::string tmp_file_basename = file_tmp_dir + filename + "-";
-
-			std::filesystem::create_directory(file_tmp_dir);
-
-			tmp_files[filename] = {};
-			tmp_files[filename].reserve(partitions);
-			for (size_t i = 0; i < partitions; ++i) {
-				auto name = tmp_file_basename + std::to_string(i);
-				auto ofs = std::make_shared<std::fstream>(name, std::ios::out);
-				tmp_files[filename].emplace_back(ofs);
+			std::string out_dir = config.out_dir;
+			if (!out_dir.ends_with('/')) {
+				out_dir += '/';
 			}
+			std::filesystem::create_directory(tmp_dir);
+			std::filesystem::create_directory(out_dir);
+			create_tmp_files(config.input_files, tmp_dir, config.partitions);
+			create_out_files(config.input_files, out_dir);
 		}
 
-		void close_tmp_files_for_writing(const std::string& filename) {
-			tmp_files.erase(filename);
+		~FileManager() {
+			std::filesystem::remove_all(tmp_dir);
 		}
 
-		void open_tmp_files_for_reading(const std::string& filename) {
-			std::string file_tmp_dir = tmp_dir + filename + '/';
-			std::string tmp_file_basename = file_tmp_dir + filename + "-";
-
-			tmp_files[filename] = {};
-			tmp_files[filename].reserve(partitions);
-			for (size_t i = 0; i < partitions; ++i) {
-				auto name = tmp_file_basename + std::to_string(i);
-				auto ifs = std::make_shared<std::fstream>(name, std::ios::in);
-				tmp_files[filename].emplace_back(ifs);
-			}
-		}
-
-		void close_tmp_files_for_reading(const std::string& filename) {
-			tmp_files.erase(filename);
-		}
-
-		TempFiles& tmp_files_for(const std::string& filename) {
+		TmpFiles& tmp_files_for(const std::string& filename) {
 			return tmp_files[filename];
+		}
+
+		OutFile& out_file_for(const std::string& filename) {
+			return out_files[filename];
+		}
+	private:
+		void create_tmp_files(const std::vector<std::string>& files, const std::string& dir, size_t count) {
+			for (auto& filename : files) {
+				auto filepath = std::filesystem::path(filename);
+				auto base = filepath.filename().replace_extension("").string();
+				auto extension = filepath.extension().string();
+				auto subdir = dir + base + '/';
+				std::filesystem::create_directory(subdir);
+
+				tmp_files[filename] = {};
+				tmp_files[filename].reserve(count);
+				for (size_t i = 0; i < count; ++i) {
+					auto name = subdir + base + '-' + std::to_string(i) + extension;
+					tmp_files[filename].emplace_back(
+						std::make_shared<std::fstream>(name,
+							std::ios::in | std::ios::end | std::ios::app)
+					);
+				}
+			}
+		}
+
+		void create_out_files(const std::vector<std::string>& files, const std::string& dir) {
+			for (auto& filename : files) {
+				auto filepath = std::filesystem::path(filename);
+				auto no_path = filepath.filename().string();
+				auto name = dir + no_path;
+				out_files[filename] = std::make_shared<std::ofstream>(name);
+			}
 		}
 	private:
 		std::string tmp_dir;
-		size_t partitions;
-		std::unordered_map<std::string, TempFiles> tmp_files;
+		std::unordered_map<std::string, TmpFiles> tmp_files;
+		std::unordered_map<std::string, OutFile> out_files;
 	};
 }
 
